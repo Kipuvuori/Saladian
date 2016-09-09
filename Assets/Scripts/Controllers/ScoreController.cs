23b5b02081ddd39a0f7fe6d8ac578126a778a8a6
Santeri Hetekivi <santeri@hetekivi.com>
2016-09-09 14:25:47 +0200

    Start of the score

---
+++ Assets/Scripts/Controllers/ScoreController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class ScoreController : MonoBehaviour {
+
+    public Text score;
+    private const string restUrl = "www.hetekivi.com/Saladian/index.php";
+
+	// Use this for initialization
+	void Start () {
+        this.score.text = getData();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    string getData()
+    {
+        WWW www = new WWW(restUrl);
+        return www.text;
+    }
+
+    static void sendScore(int score, string name)
+    {
+
+    }
+}
