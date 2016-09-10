a0e5dbeb8259c132a79aa60d92eaa42151257668
Santeri Hetekivi <santeri@hetekivi.com>
2016-09-10 10:42:27 +0300

    Rest framework commands work

--- Assets/Scripts/Controllers/ScoreController.cs
+++ Assets/Scripts/Controllers/ScoreController.cs
@@ -1,29 +1,110 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System;
+
 public class ScoreController : MonoBehaviour {
 
+    public Text list;
     public Text score;
-    private const string restUrl = "www.hetekivi.com/Saladian/index.php";
+    public Button send_score;
+    private const string restUrl = "http://vanillasky.ddns.me:5555";
+    public float current_score = 0.0f;
+    public const int max_list_size = 3;
+    private string data = "";
+
 
 	// Use this for initialization
 	void Start () {
-        this.score.text = getData();
-	}
+        Debug.Log("Starting");
+        StartCoroutine("getData");
+        Debug.Log("Called");
+        send_score.onClick.AddListener(() => sendScore());
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+        current_score += Time.deltaTime;
+        this.score.text = this.currentScore().ToString();
 
-    string getData()
-    {
-        WWW www = new WWW(restUrl);
-        return www.text;
     }
 
-    static void sendScore(int score, string name)
+    int currentScore()
     {
+        return ((int)Math.Floor(current_score));
+    }
 
+
+
+    IEnumerator getData()
+    {
+        WWW www = new WWW(restUrl);   // UTF-8 encoded json file on the server
+        yield return www;
+        if (string.IsNullOrEmpty(www.error))
+        {
+            this.updateScoreList(this.Processjson(www.text));
+        }
+        else Debug.Log(www.error);
+    }
+
+    private void updateScoreList(List<Score> scores)
+    {
+        string list = "";
+        this.list.text = list;
+        for (int i = 0; i < ScoreController.max_list_size && i < scores.Count; ++i)
+        {
+            list += i+1 + ". " + scores[i].score + ": " + scores[i].name + ", ";
+        }
+        list = list.TrimEnd(' ');
+        list = list.TrimEnd(',');
+        this.list.text = list;
+    }
+
+    void sendScore(int score, string name)
+    {
+        this.data = "/create?name="+ WWW.EscapeURL(name) + 
+            "&score="+ WWW.EscapeURL(score.ToString()) + "";
+        StartCoroutine("sendData");
+    }
+
+    IEnumerator sendData()
+    {
+        string url = restUrl + this.data;
+        Debug.Log(url);
+        WWW www = new WWW(url);
+        yield return www;
+        if (string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log("sendData - OK");
+        }
+        else Debug.Log(www.error);
+    }
+
+    void sendScore()
+    {
+        string name = "Santeri Hetekivi";
+        int score = this.currentScore();
+        this.sendScore(score, name);
+    }
+
+    private List<Score> Processjson(string jsonString)
+    {
+        jsonString =jsonString.TrimStart('[');
+        jsonString = jsonString.TrimEnd(']');
+        string[] jsons = jsonString.Split('}');
+        List<Score> scores = new List<Score>();
+        string str = "";
+        foreach (string json in jsons)
+        {
+            str = json.TrimStart(',') + "}";
+            if (str.Length > 5)
+            {
+                scores.Add(JsonUtility.FromJson<Score>(str));
+                if (scores.Count >= ScoreController.max_list_size) break;
+            }
+        }
+        return scores;
     }
 }
