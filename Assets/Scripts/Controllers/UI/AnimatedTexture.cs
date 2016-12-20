e370c79a82305b8e68113a280585df96059fa431
Anton Lehmus <anton.lehmus@gmail.com>
2016-12-20 15:58:25 +0200

    use better space BG texture

--- Assets/Scripts/Controllers/UI/AnimatedTexture.cs
+++ Assets/Scripts/Controllers/UI/AnimatedTexture.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class AnimatedTexture : MonoBehaviour {
 
     public Vector2 speed = Vector2.zero;
+    public Vector2 MaxSpeed = Vector2.zero;
+
 
     private Vector2 offset = Vector2.zero;
     private Material material;
@@ -18,6 +20,10 @@ public class AnimatedTexture : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        var nextSpeed = ScoreController.current_score / 100f;
+        if (speed.y < nextSpeed && nextSpeed < MaxSpeed.y) {
+            speed.y = nextSpeed;
+        }
         offset += speed * Time.deltaTime;
 
         material.SetTextureOffset("_MainTex", offset);
--- Assets/Scripts/Controllers/UI/TiledBackground.cs
+++ Assets/Scripts/Controllers/UI/TiledBackground.cs
@@ -2,18 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TiledBackground : MonoBehaviour {
+public class TiledBackground : MonoBehaviour
+{
 
     public int textureSize = 64;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
+        Resize();
+    }
+
+    public void Resize() {
         var newWidth = Mathf.Ceil(Screen.width / (textureSize * CameraController.scale));
         var newHeight = Mathf.Ceil(Screen.height / (textureSize * CameraController.scale));
 
-        transform.localScale = new Vector3(newWidth*textureSize,newHeight * textureSize, 1);
+        transform.localScale = new Vector3(newWidth * textureSize, newHeight * textureSize, 1);
 
         GetComponent<Renderer>().material.mainTextureScale = new Vector3(newWidth, newHeight, 1);
     }
+
 
 }
