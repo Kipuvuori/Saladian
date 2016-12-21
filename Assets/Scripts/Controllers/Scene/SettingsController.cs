fa546ea4f7fb0235e9631109604fee407ec157f1
Santeri Hetekivi <santeri@hetekivi.com>
2016-12-21 16:43:38 +0200

    Base for Settings menu.

---
+++ Assets/Scripts/Controllers/Scene/SettingsController.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
+public class SettingsController : SceneController {
+
+    public GameObject return_go;
+    private UIButton return_button;
+
+    public GameObject list;
+
+    protected new void Awake()
+    {
+        base.Awake();
+        this.addMainCamera();
+        this.addInputs();
+    }
+
+    // Use this for initialization
+    protected new void Start()
+    {
+        base.Start();
+        this.resolution = this.camera_controller.resolution;
+        this.return_button = new UIButton(this.return_go, "Return", LeaderboardController.Quit);
+    }
+
+    // Update is called once per frame
+    protected new void Update()
+    {
+        base.Update();
+        if (!this.resolution.Equals(this.camera_controller.resolution))
+        {
+            this.onResolutionChanged();
+            this.resolution = this.camera_controller.resolution;
+        }
+
+    }
+
+    public new static void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    private void addMainCamera()
+    {
+        GameObject prefab = (GameObject)Resources.Load("Prefabs/MenuCamera", typeof(GameObject));
+        LeaderboardController.Camera = Instantiate(prefab);
+        this.camera_controller = LeaderboardController.Camera.GetComponent<CameraController>();
+    }
+
+    public override void onResolutionChanged()
+    {
+        this.list.GetComponent<GridController>().onResolutionChanged();
+		if(this.camera_controller != null) this.camera_controller.onResolutionChanged ();
+
+    }
+}
