66d086c68b1578c00f8ebff0abf0446cf667fe96
Santeri Hetekivi <santeri@hetekivi.com>
2016-12-21 16:53:11 +0200

    Navigation to Setting menu working.

--- Assets/Scripts/Controllers/Scene/MenuController.cs
+++ Assets/Scripts/Controllers/Scene/MenuController.cs
@@ -9,13 +9,17 @@ public class MenuController : SceneController {
 	public GameObject start_game_go;
 	private UIButton start_game_button;
 
-	public GameObject quit_game_go;
-	private UIButton quit_game_button;
+    public GameObject settings_go;
+    private UIButton settings_button;
 
     public GameObject leaderboard_go;
     private UIButton leaderboard_button;
 
-	public GameObject menuPanel;
+    public GameObject quit_game_go;
+    private UIButton quit_game_button;
+
+
+    public GameObject menuPanel;
 
     protected new void Awake(){
 		base.Awake();
@@ -28,13 +32,14 @@ public class MenuController : SceneController {
 		base.Start ();
 		this.resolution = this.camera_controller.resolution;
 		Sprite sprite = Resources.Load<Sprite>("Sprites/explosion");
-		//this.background_image.sprite = sprite;
-		//this.background_image.color = Color.red;
-		this.start_game_button = new UIButton(this.start_game_go,"Start game", this.start_game);
-		this.quit_game_button = new UIButton(this.quit_game_go, "Quit game", MenuController.Quit);
+        //this.background_image.sprite = sprite;
+        //this.background_image.color = Color.red;
         string crown = '\u2654'.ToString();
         string leaderboard_text = crown + " Leaderboard " + crown;
+        this.start_game_button = new UIButton(this.start_game_go,"Start game", this.start_game);
         this.leaderboard_button = new UIButton(this.leaderboard_go, leaderboard_text, this.open_leaderboard);
+        this.settings_button = new UIButton(this.settings_go, "Settings", this.open_settings);
+        this.quit_game_button = new UIButton(this.quit_game_go, "Quit game", MenuController.Quit);
     }
 
     // Update is called once per frame
@@ -54,6 +59,11 @@ public class MenuController : SceneController {
     private void open_leaderboard()
     {
         SceneManager.LoadScene("Leaderboard");
+    }
+
+    private void open_settings()
+    {
+        SceneManager.LoadScene("Settings");
     }
 
     public new static void Quit()
